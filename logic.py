d862f1aaa2cc9424195f023b1a40e7a486dc0d6c
Ivan <iaromashov@gmail.com>
2020-08-13 21:24:56 +0300

    Create class Game
    Move all logic variables and methods to class

--- logic.py
+++ logic.py
@@ -1,24 +1,25 @@
-current_symbol = 'X'
+class Game:
+	def __init__(self):
+		self.current_symbol = 'X'
+		self.game_stats = {
+			"1-A": " ",
+			"1-B": " ",
+			"1-C": " ",
+			"2-A": " ",
+			"2-B": " ",
+			"2-C": " ",
+			"3-A": " ",
+			"3-B": " ",
+			"3-C": " "
+		}
 
-game_stats = {
-	"1-A": " ",
-	"1-B": " ",
-	"1-C": " ",
-	"2-A": " ",
-	"2-B": " ",
-	"2-C": " ",
-	"3-A": " ",
-	"3-B": " ",
-	"3-C": " "
-}
+	def put(self, postition):
+		if self.game_stats[postition] != " ":
+			return False
+			
+		self.game_stats[postition] = self.current_symbol
+		self.current_symbol = (lambda: 'X' if self.current_symbol == 'O' else 'O')()
+		return True
 
-def put(postition):
-	if game_stats[postition] != " ":
-		return False
-	global current_symbol
-	game_stats[postition] = current_symbol
-	current_symbol = (lambda: 'X' if current_symbol == 'O' else 'O')()
-	return True
-
-def have_winner():
-	return (game_stats["1-A"] == game_stats["1-B"] == game_stats["1-C"] != " ") or (game_stats["2-A"] == game_stats["2-B"] == game_stats["2-C"] != " ") or (game_stats["3-A"] == game_stats["3-B"] == game_stats["3-C"] != " ") or (game_stats["1-A"] == game_stats["2-A"] == game_stats["3-A"] != " ") or (game_stats["1-B"] == game_stats["2-B"] == game_stats["3-B"] != " ") or (game_stats["1-A"] == game_stats["2-B"] == game_stats["3-C"] != " ") or (game_stats["1-C"] == game_stats["2-B"] == game_stats["3-A"] != " ")
+	def have_winner(self):
+		return (self.game_stats["1-A"] == self.game_stats["1-B"] == self.game_stats["1-C"] != " ") or (self.game_stats["2-A"] == self.game_stats["2-B"] == self.game_stats["2-C"] != " ") or (self.game_stats["3-A"] == self.game_stats["3-B"] == self.game_stats["3-C"] != " ") or (self.game_stats["1-A"] == self.game_stats["2-A"] == self.game_stats["3-A"] != " ") or (self.game_stats["1-B"] == self.game_stats["2-B"] == self.game_stats["3-B"] != " ") or (self.game_stats["1-A"] == self.game_stats["2-B"] == self.game_stats["3-C"] != " ") or (self.game_stats["1-C"] == self.game_stats["2-B"] == self.game_stats["3-A"] != " ")
--- out.py
+++ out.py
@@ -10,11 +10,11 @@ def clear_console():
 	else:
 		os.system('cls')
 
-def print_grid():
+def print_grid(game):
 	print("  1 2 3")
-	print(f"A {game_stats['1-A']} {game_stats['2-A']} {game_stats['3-A']}")
-	print(f"B {game_stats['1-B']} {game_stats['2-B']} {game_stats['3-B']}")
-	print(f"C {game_stats['1-C']} {game_stats['2-C']} {game_stats['3-C']}")
+	print(f"A {game.game_stats['1-A']} {game.game_stats['2-A']} {game.game_stats['3-A']}")
+	print(f"B {game.game_stats['1-B']} {game.game_stats['2-B']} {game.game_stats['3-B']}")
+	print(f"C {game.game_stats['1-C']} {game.game_stats['2-C']} {game.game_stats['3-C']}")
 	print("")
 
 def get_move():
@@ -25,30 +25,29 @@ def get_move():
 	return move
 
 if __name__ == "__main__":
+	game = Game()
 	is_linux = platform.system()
 	counter = 0
 	
-	while not have_winner():
+	while not game.have_winner():
 		clear_console()
 
-		print_grid()
+		print_grid(game)
 
 		if counter == 9:
 			print("Draw")
 			break
 
-		print(f"Now {current_symbol} move")
+		print(f"Now {game.current_symbol} move")
 		print("*Move must be printed in format DIGIT-SYMBOL where DIGIT is column, SYMBOL is row as on the above grid")
 
 		move = get_move()
 
-		while not put(move):
+		while not game.put(move):
 			print("Wrong move")
 			move = get_move()
 			
 
-		current_symbol = (lambda: 'X' if current_symbol == 'O' else 'O')()
-
 		counter += 1
 	else:
-		print(f"Winner is {(lambda: 'X' if current_symbol == 'O' else 'O')()}")
+		print(f"Winner is {(lambda: 'X' if game.current_symbol == 'O' else 'O')()}")
