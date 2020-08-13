04fe0dd9bc45012a8631d17e184b528995fc8c6f
Ivan <iaromashov@gmail.com>
2020-08-13 20:31:34 +0300

    Add draw

--- out.py
+++ out.py
@@ -6,14 +6,25 @@ from logic import *
 
 if __name__ == "__main__":
 	is_linux = platform.system()
+	counter = 0
 	
 	while not have_winner():
+		#clearing
+		if is_linux:
+			os.system('clear')
+		else:
+			os.system('cls')
+			
 		#game stats
 		print("  1 2 3")
 		print(f"A {game_stats['1-A']} {game_stats['2-A']} {game_stats['3-A']}")
 		print(f"B {game_stats['1-B']} {game_stats['2-B']} {game_stats['3-B']}")
 		print(f"C {game_stats['1-C']} {game_stats['2-C']} {game_stats['3-C']}")
 		print("")
+
+		if counter == 9:
+			print("Draw")
+			break
 
 		print(f"Now {current_symbol} move")
 		print("*Move must be printed in format DIGIT-SYMBOL where DIGIT is column, SYMBOL is row as on the above grid")
@@ -32,12 +43,6 @@ if __name__ == "__main__":
 
 		current_symbol = (lambda: 'X' if current_symbol == 'O' else 'O')()
 
-		#clearing
-		if is_linux:
-			os.system('clear')
-		else:
-			os.system('cls')
-
-	print(f"Winner is {(lambda: 'X' if current_symbol == 'O' else 'O')()}")
-		
-
+		counter += 1
+	else:
+		print(f"Winner is {(lambda: 'X' if current_symbol == 'O' else 'O')()}")
