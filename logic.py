fb074492da35dda88e114b7d4fb427ceaa3985d2
Ivan <iaromashov@gmail.com>
2020-08-13 19:56:29 +0300

    Create base game mechanics
    logic.py:
      functions put and have_winner
    
    out.py
      printing grid

--- logic.py
+++ logic.py
@@ -1,1 +1,21 @@
- 
+current_symbol = 'X'
+
+game_stats = {
+	"1-A": " ",
+	"1-B": " ",
+	"1-C": " ",
+	"2-A": " ",
+	"2-B": " ",
+	"2-C": " ",
+	"3-A": " ",
+	"3-B": " ",
+	"3-C": " "
+}
+
+def put(postition):
+	global current_symbol
+	game_stats[postition] = current_symbol
+	current_symbol = (lambda: 'X' if current_symbol == 'O' else 'O')()
+
+def have_winner():
+	pass
--- out.py
+++ out.py
@@ -1,5 +1,38 @@
 from Color_Console import *
+import platform
+import re
+
 from logic import *
 
 if __name__ == "__main__":
-	print("Hello World!")
+	is_linux = platform.system()
+	
+	while not have_winner():
+		#game stats
+		print("  1 2 3")
+		print(f"A {game_stats['1-A']} {game_stats['2-A']} {game_stats['3-A']}")
+		print(f"B {game_stats['1-B']} {game_stats['2-B']} {game_stats['3-B']}")
+		print(f"C {game_stats['1-C']} {game_stats['2-C']} {game_stats['3-C']}")
+		print("")
+
+		print(f"Now {current_symbol} move")
+		print("*Move must be printed in format DIGIT-SYMBOL where DIGIT is column, SYMBOL is row as on the above grid")
+
+		move = input()
+		while not re.match("(1|2|3)-(A|B|C)", move):
+			print("Please observe format")
+			move = input()
+
+		put(move)
+
+		current_symbol = (lambda: 'X' if current_symbol == 'Y' else 'Y')()
+
+		#clearing
+		if is_linux:
+			os.system('clear')
+		else:
+			os.system('cls')
+
+	print(f"Winner is {current_symbol}")
+		
+
