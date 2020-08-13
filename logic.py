8f3ed0de9b4549e7e1b827244b35ee8a80737ea6
Ivan <iaromashov@gmail.com>
2020-08-13 21:40:41 +0300

    Make code pretty

--- logic.py
+++ logic.py
@@ -1,5 +1,6 @@
 class Game:
 	def __init__(self):
+		self.__move_i = 0
 		self.current_symbol = 'X'
 		self.game_stats = {
 			"1-A": " ",
@@ -16,10 +17,33 @@ class Game:
 	def put(self, postition):
 		if self.game_stats[postition] != " ":
 			return False
-			
+
 		self.game_stats[postition] = self.current_symbol
-		self.current_symbol = (lambda: 'X' if self.current_symbol == 'O' else 'O')()
+		self.current_symbol = self.__change_symbol(self.current_symbol)
+		self.__move_i += 1
 		return True
 
 	def have_winner(self):
 		return (self.game_stats["1-A"] == self.game_stats["1-B"] == self.game_stats["1-C"] != " ") or (self.game_stats["2-A"] == self.game_stats["2-B"] == self.game_stats["2-C"] != " ") or (self.game_stats["3-A"] == self.game_stats["3-B"] == self.game_stats["3-C"] != " ") or (self.game_stats["1-A"] == self.game_stats["2-A"] == self.game_stats["3-A"] != " ") or (self.game_stats["1-B"] == self.game_stats["2-B"] == self.game_stats["3-B"] != " ") or (self.game_stats["1-A"] == self.game_stats["2-B"] == self.game_stats["3-C"] != " ") or (self.game_stats["1-C"] == self.game_stats["2-B"] == self.game_stats["3-A"] != " ")
+
+	def have_draw(self):
+		return self.__move_i == 9
+
+	def get_winner(self):
+		if not self.have_winner():
+			raise Exception("Have not winner yet")
+
+		return self.__change_symbol(self.current_symbol)
+
+	def print_grid(self):
+		print("  1 2 3")
+		print(f"A {self.game_stats['1-A']} {self.game_stats['2-A']} {self.game_stats['3-A']}")
+		print(f"B {self.game_stats['1-B']} {self.game_stats['2-B']} {self.game_stats['3-B']}")
+		print(f"C {self.game_stats['1-C']} {self.game_stats['2-C']} {self.game_stats['3-C']}")
+
+	def __change_symbol(self, symbol):
+		if symbol == 'X':
+			return 'O'
+		else:
+			return 'X'
+
--- out.py
+++ out.py
@@ -10,16 +10,9 @@ def clear_console():
 	else:
 		os.system('cls')
 
-def print_grid(game):
-	print("  1 2 3")
-	print(f"A {game.game_stats['1-A']} {game.game_stats['2-A']} {game.game_stats['3-A']}")
-	print(f"B {game.game_stats['1-B']} {game.game_stats['2-B']} {game.game_stats['3-B']}")
-	print(f"C {game.game_stats['1-C']} {game.game_stats['2-C']} {game.game_stats['3-C']}")
-	print("")
-
 def get_move():
 	move = input()
-	while not re.match("(1|2|3)-(A|B|C)", move):
+	while not re.match("(1|2|3)-(a|b|c)", lower(move)):
 		print("Please observe format")
 		move = input()
 	return move
@@ -27,16 +20,12 @@ def get_move():
 if __name__ == "__main__":
 	game = Game()
 	is_linux = platform.system()
-	counter = 0
 	
-	while not game.have_winner():
+	while not game.have_winner() and not game.have_draw():
 		clear_console()
 
-		print_grid(game)
-
-		if counter == 9:
-			print("Draw")
-			break
+		game.print_grid()
+		print("")
 
 		print(f"Now {game.current_symbol} move")
 		print("*Move must be printed in format DIGIT-SYMBOL where DIGIT is column, SYMBOL is row as on the above grid")
@@ -46,8 +35,13 @@ if __name__ == "__main__":
 		while not game.put(move):
 			print("Wrong move")
 			move = get_move()
-			
 
-		counter += 1
 	else:
-		print(f"Winner is {(lambda: 'X' if game.current_symbol == 'O' else 'O')()}")
+		clear_console()
+		game.print_grid()
+		print("")
+		
+		if game.have_winner():
+			print(f"Winner is {game.get_winner()}")
+		else:
+			print("Draw")
